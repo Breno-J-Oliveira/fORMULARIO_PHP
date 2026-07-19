98a35050df1b55b4ca630b083ef3f7fdda1a28b0
Breno <breno.emailsenai@gmail.com>
2026-07-18 22:00:49 -0300

    Delete comecodophp directory

--- comecodophp/atividade1/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$nome = 'raul';
-$raul = 'mago';
-
-//esse funlçao serve para mostrar coisas na tela
-echo 'hello '. $$nome;
-
-?>
--- comecodophp/atividade2/igual.php
+++
@@ -1,8 +0,0 @@
-    <?php
-    $idade = "20";
-
-    if ($idade == "18") {
-        // código a ser executado se a condição for verdadeira
-        echo "voce pode dirigir";
-    } 
-    ?>
